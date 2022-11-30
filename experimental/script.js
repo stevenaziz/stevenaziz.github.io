28e27c12c37faf94aef843932169b7b7699a7d84
Steven Aziz <16842077+stevenaziz@users.noreply.github.com>
2022-11-30 04:40:29 -0800

    Relocated all JavaScript to separate file

---
+++ experimental/script.js
@@ -0,0 +1,34 @@
+// Toggles the mobile nav dropdown
+function ToggleDropdown() {
+    document.body.classList.toggle("fullscreen-nav");
+}
+
+// Checks whether the mobile nav dropdown is open and closes it
+function CloseDropdown() {
+    if (document.body.classList.contains("fullscreen-nav")) {
+        ToggleDropdown();
+    }
+}
+
+// Applies mobile class to body if viewport is too small
+function CheckViewport() {
+    if (window.innerWidth < 768) {
+        document.body.classList.add("mobile");
+    }
+    else {
+        CloseDropdown();
+        document.body.classList.remove("mobile");
+    }
+}
+
+// Function is run when the page initially loads
+function Main() {
+    CheckViewport();
+    
+    const mobileLinks = document.querySelectorAll("nav>div>a");
+    for (anchor of mobileLinks) {
+        anchor.addEventListener('click', CloseDropdown);
+    }
+
+    document.body.classList.remove("fade-out");
+}
