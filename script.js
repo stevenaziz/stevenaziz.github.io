c74ebfe7751aab2db947935dafb81d4c7d4a0c49
stevenaziz <16842077+stevenaziz@users.noreply.github.com>
2025-06-18 19:55:22 -0700

    Minor code readability changes

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 // Toggles the mobile nav dropdown
-function ToggleDropdown() {
+function toggleDropdown() {
     document.body.classList.toggle("fullscreen-nav");
 }
 
 // Checks whether the mobile nav dropdown is open and closes it
-function CloseDropdown() {
+function closeDropdown() {
     if (document.body.classList.contains("fullscreen-nav")) {
-        ToggleDropdown();
+        toggleDropdown();
     }
 }
 
 // Applies mobile class to body if viewport is too small
-function CheckViewport() {
+function checkViewport() {
     let breakpoint = 960;
 
     if (window.innerWidth < breakpoint && !document.body.classList.contains("mobile")) {
         document.body.classList.add("mobile");
     }
     else if (window.innerWidth >= breakpoint && document.body.classList.contains("mobile")) {
-        CloseDropdown();
+        closeDropdown();
         document.body.classList.remove("mobile");
     }
 }
 
 // Function is run when the page initially loads
-function Main() {
-    CheckViewport();
+function main() {
+    checkViewport();
     
     const mobileLinks = document.querySelectorAll("nav>div>a");
+
     for (anchor of mobileLinks) {
-        anchor.addEventListener('click', CloseDropdown);
+        anchor.addEventListener('click', closeDropdown);
     }
 
     document.getElementById("copyright").innerHTML = "Copyright &copy; " + new Date().getFullYear() + " Steven Aziz. All rights reserved.";
